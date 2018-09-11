355ddb04d2760e49175353dd409bf195ca873e0c
shaokeke <499646194@163.com>
2018-09-11 17:36:24 +0800

    Auto translate slug

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -25,6 +25,11 @@ class TopicsController extends Controller
 
     public function show(Topic $topic)
     {
+        // URL 矫正
+        if ( ! empty($topic->slug) && $topic->slug != $request->slug) {
+            return redirect($topic->link(), 301);
+        }
+
         return view('topics.show', compact('topic'));
     }
 
@@ -40,7 +45,7 @@ class TopicsController extends Controller
         $topic->user_id = Auth::id();
         $topic->save();
 
-        return redirect()->route('topics.show', $topic->id)->with('success', '创建成功');
+        return redirect()->to($topic->link())->with('success', '创建成功');
     }
 
 	public function edit(Topic $topic)
@@ -58,7 +63,7 @@ class TopicsController extends Controller
 
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('success', '更新成功');
+		return redirect()->to($topic->link())->with('success', '更新成功');
 	}
 
 	public function destroy(Topic $topic)
