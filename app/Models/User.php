6cbff0c830c2bd058b4bd4f5a53f0d2e142a41d4
shaokeke <499646194@163.com>
2018-09-12 16:23:21 +0800

    List user replies

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,8 +31,14 @@ class User extends Authenticatable
     {
         return $this->hasMany(Topic::class);
     }
+
     public function isAuthorOf($model)
     {
         return $this->id == $model->user_id;
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,4 @@ Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload
 
 //seo
 Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
+Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
